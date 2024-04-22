8cf4410eb5133a3722b364e4725b380ab01ffbca
Franc Mihani <francimihani@gmail.com>
2024-04-22 23:03:17 +0200

    chore: install tailwind

--- src/views/users/UserIndex.vue
+++ src/views/users/UserIndex.vue
@@ -2,10 +2,12 @@
 import useIndexQuery from "@/composables/useIndexQuery"
 import UsersService from "@/services/users"
 import { useToast } from "vue-toastification"
+import SimpleTable from "@/components/tables/SimpleTable.vue";
 
 const { items } = useIndexQuery(UsersService.all)
 const toast = useToast()
 </script>
 
 <template>
+  <SimpleTable />
 </template>
